51d79950f44927579e6dbfd081473814d3a9ffc0
Thomas Lefebvre <thomas.lefebvre@student-cs.fr>
2021-08-16 17:00:23 +0200

    Trying to connect to Mongo

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 import Express from "express"
 import Connection from "./database.js"
+import { MongoClient } from "mongodb";
+import assert from "assert"
 
 const app = Express()
 const port = 3000
+
+const url = 'mongodb://localhost:11341/test'
+
 app.use(Express.json()) //Allows to parse json data into a post method
 app.use(Express.urlencoded({ extended: true }))
 
@@ -23,6 +28,39 @@ app.post('/add', (req, res) => {
         console.log(error)
         res.status(500).send({ msg: 'Ooops'})
     }
+})
+
+app.post('/addMongo', (req, res) => {
+    console.log(req.body)
+    const item = {
+        title: req.body.title,
+        content: req.body.content,
+        author: req.body.author
+    }
+    MongoClient.connect(url, (err, db) => {
+        assert.equal(null, err)
+        db.collection('rpayUser').insertOne(item, (err, result) => {
+            assert.equal(null, err)
+            console.log(result)
+            db.close();
+        })
+    })
+})
+
+app.get('/getMongo', (req, res) => {
+    var resultArray = []
+    MongoClient.connect(url, (err, db) => {
+        assert.equal(null, err)
+        const cursor = db.collection('rpayUser').find()
+        cursor.forEach((doc, err) => {
+            assert.equal(null, err)
+            resultArray.push(doc)
+        }, () => {
+            db.close()
+            console.log(resultArray)
+            res.render(resultArray)
+        });
+    })
 })
 
 app.listen(port, () => console.log("Listening on port " + port))
